969323340cbac69c5fbe13ca58613c10fa405785
Johnlin <johno1001741060168@gmail.com>
2025-10-16 15:37:52 +0800

    v1.4.0 fix: 更新版本號至 v1.4.0

--- app/utils/siteConfigs.ts
+++ app/utils/siteConfigs.ts
@@ -1,5 +1,5 @@
 export const SITE_CONFIG = {
-  version: '1.3.3 Beta',
+  version: '1.4.0',
   name: '中臺機器人研究社',
   shortName: '中臺機器人研究社',
   url: process.env.NEXT_PUBLIC_SITE_URL || 'https://robotctust.com',
